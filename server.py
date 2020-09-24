c4f3c01e88d4a8d33db88370dd69e05dacb4afe3
kheina <vixen@kheina.com>
2020-09-24 18:53:55 -0400

    add localhost

--- server.py
+++ server.py
@@ -165,7 +165,7 @@ from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.routing import Route, Mount
 
 middleware = [
-	Middleware(TrustedHostMiddleware, allowed_hosts={ '127.0.0.1:5002', 'tags.kheina.com', 'tags-dev.kheina.com' }),
+	Middleware(TrustedHostMiddleware, allowed_hosts={ 'localhost', '127.0.0.1', 'tags.kheina.com', 'tags-dev.kheina.com' }),
 ]
 
 routes = [
