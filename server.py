a335e48dc00be30da65d0cd4a8b7dcbab47dbb5f
kheina <vixen@kheina.com>
2020-09-24 18:46:38 -0400

    add server

---
+++ server.py
@@ -0,0 +1,188 @@
+from kh_common.exceptions import checkJsonKeys, JsonErrorHandler
+from starlette.responses import UJSONResponse
+from kh_common.auth import AuthenticatedAsync
+from kh_common.logging import getLogger
+from traceback import format_tb
+from tagger import Tagger
+import time
+
+
+logger = getLogger()
+tagger = Tagger()
+
+
+@JsonErrorHandler()
+@AuthenticatedAsync()
+async def v1AddTags(req, token_data={ }) :
+	"""
+	{
+		"post_id": str,
+		"tags": [
+			str
+		]
+	}
+	"""
+	requestJson = await req.json()
+	checkJsonKeys(requestJson, ['post_id', 'tags'])
+
+	return UJSONResponse(
+		tagger.removeTags(
+			token_data['data']['user_id'],
+			requestJson['post_id'],
+			tuple(requestJson['tags']),
+		)
+	)
+
+
+@JsonErrorHandler()
+@AuthenticatedAsync()
+async def v1RemoveTags(req, token_data={ }) :
+	"""
+	{
+		"post_id": str,
+		"tags": [
+			str
+		]
+	}
+	"""
+	requestJson = await req.json()
+	checkJsonKeys(requestJson, ['post_id', 'tags'])
+
+	return UJSONResponse(
+		tagger.removeTags(
+			token_data['data']['user_id'],
+			requestJson['post_id'],
+			tuple(requestJson['tags']),
+		)
+	)
+
+
+@JsonErrorHandler()
+@AuthenticatedAsync()
+async def v1InheritTag(req, token_data={ }) :
+	"""
+	{
+		"parent_tag": str,
+		"child_tag": str,
+		"deprecate": Optional[bool],
+		"admin": Optional[bool]
+	}
+	"""
+	requestJson = await req.json()
+	checkJsonKeys(requestJson, ['parent_tag', 'child_tag'])
+
+	return UJSONResponse(
+		tagger.inheritTag(
+			token_data['data']['user_id'],
+			requestJson['parent_tag'],
+			requestJson['child_tag'],
+			requestJson.get('deprecate'),
+			token_data['data'].get('admin'),
+		)
+	)
+
+
+@JsonErrorHandler()
+@AuthenticatedAsync()
+async def v1UpdateTag(req, token_data={ }) :
+	"""
+	{
+		"tag": str,
+		"tag_class": Optional[str],
+		"owner": Optional[str],
+		"admin": Optional[bool]
+	}
+	"""
+	requestJson = await req.json()
+	checkJsonKeys(requestJson, ['tag', 'tag_class'])
+
+	return UJSONResponse(
+		tagger.updateTag(
+			token_data['data']['user_id'],
+			requestJson['tag'],
+			requestJson['tag_class'],
+			requestJson.get('owner'),
+			token_data['data'].get('admin'),
+		)
+	)
+
+
+@JsonErrorHandler()
+@AuthenticatedAsync()
+async def v1FetchTags(req, token_data={ }) :
+	"""
+	{
+		"post_id": str
+	}
+	"""
+	requestJson = await req.json()
+	checkJsonKeys(requestJson, ['post_id'])
+
+	return UJSONResponse(
+		tagger.fetchTagsByPost(
+			token_data['data']['user_id'],
+			requestJson['post_id'],
+		)
+	)
+
+
+async def v1Help(req) :
+	return UJSONResponse({
+		'/v1/create_post': {
+			'auth': {
+				'required': True,
+				'user_id': 'int',
+			},
+		},
+		'/v1/upload_image': {
+			'auth': {
+				'required': True,
+				'user_id': 'int',
+			},
+			'file': 'image',
+			'post_id': 'Optional[str]',
+		},
+		'/v1/update_post': {
+			'auth': {
+				'required': True,
+				'user_id': 'int',
+			},
+			'privacy': 'Optional[str]',
+			'title': 'Optional[str]',
+			'description': 'Optional[str]',
+		},
+	})
+
+
+async def shutdown() :
+	uploader.close()
+
+
+from starlette.applications import Starlette
+from starlette.staticfiles import StaticFiles
+from starlette.middleware import Middleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+from starlette.routing import Route, Mount
+
+middleware = [
+	Middleware(TrustedHostMiddleware, allowed_hosts={ '127.0.0.1:5002', 'tags.kheina.com', 'tags-dev.kheina.com' }),
+]
+
+routes = [
+	Route('/v1/add_tags', endpoint=v1AddTags, methods=('POST',)),
+	Route('/v1/remove_tags', endpoint=v1RemoveTags, methods=('POST',)),
+	Route('/v1/inherit_tag', endpoint=v1InheritTag, methods=('POST',)),
+	Route('/v1/update_tag', endpoint=v1UpdateTag, methods=('POST',)),
+	Route('/v1/fetch_tags', endpoint=v1FetchTags, methods=('POST',)),
+	Route('/v1/help', endpoint=v1Help, methods=('GET',)),
+]
+
+app = Starlette(
+	routes=routes,
+	middleware=middleware,
+	on_shutdown=[shutdown],
+)
+
+if __name__ == '__main__' :
+	from uvicorn.main import run
+	run(app, host='127.0.0.1', port=5002)
--- tagger.py
+++ tagger.py
@@ -1,36 +1,22 @@
-from kh_common.exceptions.http_error import BadRequest, InternalServerError
+from kh_common.exceptions.http_error import BadRequest, Forbidden, NotFound, InternalServerError
+from kh_common.caching import ArgsCache, SimpleCache
+from typing import Optional, Dict, List, Tuple
 from psycopg2.errors import UniqueViolation
-from kh_common.caching import SimpleCache
 from kh_common.logging import getLogger
-from typing import Optional, Dict, List
 from kh_common.sql import SqlInterface
 from uuid import uuid4
 
 
 class Tagger(SqlInterface) :
 
-	def __init__(self) :
-		SqlInterface.__init__(self)
-		self.logger = getLogger('tagger')
-
-
-	def validatePostId(self, post_id: str) :
+	def _validatePostId(self, post_id: str) :
 		if len(post_id) != 8 :
 			raise BadRequest('the given post id is invalid.', logdata={ 'post_id': post_id })
 
 
-	def validatePageNumber(self, page_number: int) :
-		if page_number < 1 :
-			raise BadRequest('the given page number is invalid.', logdata={ 'page_number': page_number })
-
-
-	def validateCount(self, count: int) :
-		if count < 1 :
-			raise BadRequest('the given count is invalid.', logdata={ 'count': count })
-
-
-	def addTags(self, user_id: int, post_id: str, tags: List[str]) :
-		self.validatePostId(post_id)
+	@ArgsCache(60)
+	def addTags(self, user_id: int, post_id: str, tags: Tuple[str]) :
+		self._validatePostId(post_id)
 
 		try :
 			self.query("""
@@ -52,8 +38,9 @@ class Tagger(SqlInterface) :
 			raise InternalServerError('an error occurred while adding tags for provided post.', logdata=logdata)
 
 
-	def removeTags(self, user_id: int, post_id: str, tags: List[str]) :
-		self.validatePostId(post_id)
+	@ArgsCache(60)
+	def removeTags(self, user_id: int, post_id: str, tags: Tuple[str]) :
+		self._validatePostId(post_id)
 
 		try :
 			self.query("""
@@ -75,35 +62,8 @@ class Tagger(SqlInterface) :
 			raise InternalServerError('an error occurred while removing tags for provided post.', logdata=logdata)
 
 
-	def fetchPosts(self, user_id: int, tags: List[str], count:int=64, page:int=1) :
-		self.validatePageNumber(page)
-		self.validateCount(count)
-
-		try :
-			data = self.query("""
-				SELECT kheina.public.fetch_posts_by_tag(%s, %s, %s, %s);
-				""",
-				(tags, user_id, count, count * (page - 1)),
-				fetch_all=True,
-			)
-
-		except :
-			refid = uuid4().hex
-			logdata = {
-				'refid': refid,
-				'page': page,
-				'user_id': user_id,
-				'tags': tags,
-			}
-			self.logger.exception(logdata)
-			raise InternalServerError('an error occurred while fetching posts.', logdata=logdata)
-
-		return {
-			'posts': [i[0] for i in data],
-		}
-
-
-	def inheritTag(self, user_id: int, parent_tag: str, child_tag: str, deprecate:bool=False) :
+	@ArgsCache(60)
+	def inheritTag(self, user_id: int, parent_tag: str, child_tag: str, deprecate:bool=False, admin:bool=False) :
 		try :
 			data = self.query("""
 				CALL kheina.public.inherit_tag(%s, %s, %s, %s);
@@ -125,6 +85,7 @@ class Tagger(SqlInterface) :
 			raise InternalServerError('an error occurred while adding a new tag inheritance.', logdata=logdata)
 
 
+	@ArgsCache(60)
 	def updateTag(self, user_id: int, tag: str, tag_class:str=None, owner:str=None, admin:bool=False) :
 		query = []
 		params = []
@@ -133,7 +94,9 @@ class Tagger(SqlInterface) :
 			query.append('SET class_id = tag_class_to_id(%s)')
 			params.append(tag_class)
 
-		if owner and admin :
+		if owner :
+			if not admin :
+				raise Forbidden('only admins are allowed to use this function.')
 			query.append('SET owner = user_to_id(%s)')
 			params.append(owner)
 
@@ -165,8 +128,9 @@ class Tagger(SqlInterface) :
 			raise InternalServerError('an error occurred while removing tags for provided post.', logdata=logdata)
 
 
+	@ArgsCache(60)
 	def fetchTagsByPost(self, user_id: int, post_id: str) :
-		self.validatePostId(post_id)
+		self._validatePostId(post_id)
 
 		try :
 			data = self.query("""
@@ -198,10 +162,10 @@ class Tagger(SqlInterface) :
 				post_id: dict(data),
 			}
 
-		raise BadRequest('no tags were found for the provided post.', logdata={ 'post_id': post_id })
+		raise NotFound("the provided post does not exist or you don't have access to it.", logdata={ 'post_id': post_id })
 
 
-	@SimpleCache(900)
+	@SimpleCache(60)
 	def _pullAllTags(self) :
 		try :
 			data = self.query("""
@@ -229,24 +193,22 @@ class Tagger(SqlInterface) :
 			self.logger.exception(logdata)
 			raise InternalServerError('an error occurred while retrieving tags.', logdata=logdata)
 
-		tags = { }
-		for i in data :
-			# class, tag, deprecated, children, owner
-			if i[0] in tags :
-				tags[i[0]][i[1]] = { 'deprecated': i[2], 'children': list(filter(None, i[3])), 'owner': i[4] }
-			else :
-				tags[i[0]] = { i[1]: { 'deprecated': i[2], 'children': list(filter(None, i[3])), 'owner': i[4] } }
-
-		return tags
+		return data
 
 
 	def tagLookup(self, tag:Optional[str]=None) :
 		tag = tag or ''
 
-		tags = self._pullAllTags()
+		data = self._pullAllTags()
 
-		return {
-			t: data
-			for t, data in data.items()
-			if t.startswith(tag)
-		}
+		tags = { }
+		for i in data :
+			if not i[1].startswith(tag) :
+				continue
+
+			if i[0] in tags :
+				tags[i[0]][i[1]] = { 'deprecated': i[2], 'inherited_tags': list(filter(None, i[3])), 'owner': i[4] }
+			else :
+				tags[i[0]] = { i[1]: { 'deprecated': i[2], 'inherited_tags': list(filter(None, i[3])), 'owner': i[4] } }
+
+		return tags
