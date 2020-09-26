6a6a6e82d794db45de4094a0a407dc93c2f0dad1
kheina <vixen@kheina.com>
2020-09-26 01:20:33 -0400

    forgot to push models changes

--- models.py
+++ models.py
@@ -2,13 +2,23 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
-class TagsRequest(BaseModel) :
+class PostRequest(BaseModel) :
 	post_id: str
+
+
+class TagsRequest(PostRequest) :
 	tags: List[str]
 
 
 class InheritRequest(BaseModel) :
-	parent_tag: str,
-	child_tag: str,
-	deprecate: Optional[bool],
+	parent_tag: str
+	child_tag: str
+	deprecate: Optional[bool]
+	admin: Optional[bool]
+
+
+class UpdateRequest(BaseModel) :
+	tag: str
+	tag_class: Optional[str]
+	owner: Optional[str]
 	admin: Optional[bool]
